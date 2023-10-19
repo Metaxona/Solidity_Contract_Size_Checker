9f35612681290ec9b199d3cf0fb3343c47037165
Metaxona <metaxonadev@gmail.com>
2023-10-20 05:42:28 +0800

    Fixed errors from previous commit

--- SizeChecker.sol
+++ SizeChecker.sol
@@ -3,18 +3,30 @@ pragma solidity ^0.8.20;
 
 contract SizeChecker {
 
+    uint16 constant MAX_CONTRACT_SIZE = 24576;
+    uint16 constant UINT16_MAX_VALUE = 65535;
+
     // Checks the contract size and will return a uint256 number in bytes (24576 bytes - max size of a solidity contract to be deployed)
-    function checkContractSize(address contractAddress) public view returns(uint size) {
+    function checkContractSize(address contractAddress) public view returns(uint16 size) {
         assembly{
             size := extcodesize(contractAddress)
         }
     }
 
-    function hasExceededMax(address contractAddress) public view returns(bool, uint spaceLeft){
-        unchecked {
-            spaceLeft = (checkContractSize(contractAddress) > 24576, 24576 - checkContractSize(contractAddress));
+    function hasExceededMax(address contractAddress) public view returns(bool hasExceeded, uint16 spaceLeft, uint16 exceededBy){
+
+        uint16 contractSize = checkContractSize(contractAddress);
+
+        if(contractSize > MAX_CONTRACT_SIZE){
+            exceededBy = contractSize - MAX_CONTRACT_SIZE;
+            hasExceeded = true;
         }
-        return spaceLeft;
+
+        if(contractSize <= MAX_CONTRACT_SIZE){
+            spaceLeft = MAX_CONTRACT_SIZE - contractSize;
+        }
+
+        return (hasExceeded, spaceLeft, exceededBy);
     }
 
 }
